ae2aa8a8e8682caa579afd7ac28b4336cadc8bb8
TheKK <thumbd03803@gmail.com>
2014-05-08 02:45:14 +0800

    Find the truth

--- readPic.py
+++ readPic.py
@@ -5,7 +5,7 @@ import struct
 
 def MakePixelArray(inputData, startPos, width, height):
     #Init tmp array
-    tmpArray = [width * height]
+    tmpArray = []
 
     for y in range(height):
         for x in range(width):
@@ -18,8 +18,7 @@ def MakePixelArray(inputData, startPos, width, height):
             pixel['b'] = struct.unpack_from('<B', inputData, (startPos + ((y * width) + x) * 3) + 0)[0]
 
             #Add this dictionnary into array
-            #tmpArray.append(pixel)
-            tmpArray[y * width + x] = pixel
+            tmpArray.append(pixel)
 
     return tmpArray
 
@@ -38,7 +37,32 @@ else:
 
 #Read from picture
 with open(filePath, 'rb') as fopen:
-    picData = fopen.read()
+    BMPHeader = fopen.read(14)
+    picStart = struct.unpack_from("<I", BMPHeader, 0x0A)[0]
+    print('Start= ', picStart)
+
+    BMPInfo = fopen.read(picStart - 14)
+    picWidth = struct.unpack_from('<i', BMPInfo, 4)[0]
+    picHeight = struct.unpack_from('<i', BMPInfo, 8)[0]
+
+    BMPColor = fopen.read(int(picWidth) * int(picHeight) * 3)
+    print('Width= ')
+    print(picWidth)
+    print('Height= ')
+    print(picHeight)
+
+    print('r= ', struct.unpack_from('<B', BMPColor, 0 + 2)[0])
+    print('g= ', struct.unpack_from('<B', BMPColor, 0 + 1)[0])
+    print('b= ', struct.unpack_from('<B', BMPColor, 0 + 0)[0])
+
+    print('')
+
+    print('r= ', struct.unpack_from('<B', BMPColor, picWidth*3 + 2)[0])
+    print('g= ', struct.unpack_from('<B', BMPColor, picWidth*3 + 1)[0])
+    print('b= ', struct.unpack_from('<B', BMPColor, picWidth*3 + 0)[0])
+
+
+sys.exit(1)
 
 #Get width and height of picture
 picWidth = struct.unpack_from("<i", picData, 0x12)[0]
